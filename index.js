4e5b3acd72c4a350ec7f6d9356dec812b9e80515
Maik Hummel <maik@neohelden.com>
2021-04-23 14:50:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const pkg = require('./package.json')
+const fs = require('fs/promises')
+
+async function main() {
+  const catalogueReq = await axios(
+    "https://catalogue.nodered.org/catalogue.json"
+  );
+
+  const modules = catalogueReq.data.modules;
+  pkg.dependencies = {}
+  modules.forEach(module => {
+      pkg.dependencies[module.id] = module.version
+  })
+
+  await fs.writeFile('package2.json', JSON.stringify(pkg, null, 2))
+  console.log('Done... All node-red packages have been written to package.json')
+  console.log('You should not try to run npm install on it')
+  console.log('Might take hours!')
+}
+
+main();
